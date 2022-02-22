44778351b6cf114a804df466c22f799cf54ee6ea
Wesley Stedman <wesleystedman@gmail.com>
2022-02-21 18:10:11 -0800

    Update Django settings to fix issue from out-of-date tutorial

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000'
-)
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
