2e0465b666c7474ec093e580766e923578b39ede
Wesley Stedman <wesleystedman@gmail.com>
2022-03-01 19:21:29 -0800

    Add views and paths for runs

--- backend/ironmon_stats/views.py
+++ backend/ironmon_stats/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Run, Pokemon
+from .serializers import RunSerializer, PokemonSerializer
+
+class RunList(generics.ListCreateAPIView):
+  queryset =  Run.objects.all()
+  serializer_class = RunSerializer
+
+
+class RunDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Run.objects.all()
+  serializer_class = RunSerializer
