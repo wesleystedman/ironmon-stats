a55af77749c3e3ee008c3fccf7465857a8090572
Wesley Stedman <wesleystedman@gmail.com>
2022-03-04 09:08:16 -0800

    Refactor views to use ViewSets

--- backend/ironmon_stats/views.py
+++ backend/ironmon_stats/views.py
@@ -1,40 +1,22 @@
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import permissions, viewsets
 from .models import Run, Pokemon
 from django.contrib.auth.models import User
 from .serializers import RunSerializer, PokemonSerializer, UserSerializer
 from .permissions import IsRunOwnerOrReadOnly, IsPokemonOwnerOrReadOnly
 
 
-class RunList(generics.ListCreateAPIView):
+class RunViewSet(viewsets.ModelViewSet):
   queryset =  Run.objects.all()
   serializer_class = RunSerializer
   permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsRunOwnerOrReadOnly]
 
 
-class RunDetail(generics.RetrieveUpdateDestroyAPIView):
-  queryset = Run.objects.all()
-  serializer_class = RunSerializer
-  permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsRunOwnerOrReadOnly]
-
-
-class PokemonList(generics.ListCreateAPIView):
+class PokemonViewSet(viewsets.ModelViewSet):
   queryset =  Pokemon.objects.all()
   serializer_class = PokemonSerializer
   permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsPokemonOwnerOrReadOnly]
 
 
-class PokemonDetail(generics.RetrieveUpdateDestroyAPIView):
-  queryset = Pokemon.objects.all()
-  serializer_class = PokemonSerializer
-  permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsPokemonOwnerOrReadOnly]
-
-
-class UserList(generics.ListAPIView):
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
   queryset = User.objects.all()
   serializer_class = UserSerializer
