5493e7e9bfdfa9700c5b9beda05ef025847d995b
Wesley Stedman <wesleystedman@gmail.com>
2022-03-04 09:18:44 -0800

    Refactor urls to use a router - remove format_suffix_patterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,19 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from ironmon_stats import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'runs', views.RunViewSet)
+router.register(r'pokemon', views.PokemonViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/runs/', views.RunList.as_view()),
-    path('api/runs/<int:pk>/', views.RunDetail.as_view()),
-    path('api/pokemon/', views.PokemonList.as_view()),
-    path('api/pokemon/<int:pk>/', views.PokemonDetail.as_view()),
-    path('api/users/', views.UserList.as_view()),
-    path('api/users/<int:pk>/', views.UserDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
-
-urlpatterns += [
     path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
 ]
