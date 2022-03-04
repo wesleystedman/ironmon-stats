f736152f6ce346d85b70a95aefa1d3bfedb5f757
Wesley Stedman <wesleystedman@gmail.com>
2022-03-04 09:22:51 -0800

    Remove unused import

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from ironmon_stats import views
-from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
